ac07de00eaa0a482cc4e3aeb69e36b2f7342a032
tsc00 <tsc00@iesemilidarder.com>
2018-01-09 17:43:20 +0100

    dummies

--- dummyweb/src/main/java/Launcher.java
+++ dummyweb/src/main/java/Launcher.java
@@ -1,6 +1,10 @@
 package com.iesemilidarder.fordummies.web;
 
+import com.iesemilidarder.fordummies.core.DBObject;
 import com.iesemilidarder.fordummies.core.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.Request;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +13,7 @@ import static spark.Spark.*;
 
 public class Launcher {
     private static List<User> IUser = new ArrayList<User>();
+    private static final Logger log = LoggerFactory.getLogger(Launcher.class);
 
     public static void init() {
         for (int i=0;i<10; i++){
@@ -18,6 +23,13 @@ public class Launcher {
             user.save();
             IUser.add(user);
         }
+        DBObject dbo = IUser.get(0);
+        log.info("Loading finished");
+    }
+
+    private static boolean shouldReturnHtml(Request request) {
+        String accept = request.headers("Accept");
+        return accept != null && accept.contains("text/html");
     }
     public static void  main (String... args){
         init();
